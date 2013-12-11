7f1a88acb44945d40eb404231ad4c1f9eebecd26
Gerry High <ghigh@Gerrys-MacBook-Pro.local>
2013-12-10 23:28:28 -0600

    Initial Commit

---
+++ BitCoinMainActivity.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Android.App;
+using Android.OS;
+using Android.Speech.Tts;
+using Com.Google.Android.Glass.App;
+
+namespace GlassGetBitcoinPrice
+{
+
+	[Activity(Label = "BitCoin", MainLauncher = true, Icon = "@drawable/icon")]
+	[IntentFilter(new[] { "com.google.android.glass.action.VOICE_TRIGGER" })]
+	[MetaData("com.google.android.glass.VoiceTrigger", Resource = "@xml/voice_trigger_start")]
+	public class BitCoinMainActivity : Activity, TextToSpeech.IOnInitListener
+    {
+		private TextToSpeech 	tts;
+		private int 			result = -1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+			tts = new TextToSpeech(this, this);
+
+			var card = new Card(this);
+			card.SetText("Fetching Bitcoin price");
+			card.SetInfo("Source: bitstamp.net");
+            SetContentView(card.ToView());
+
+			GetPrice();
+        }
+
+		/// <summary>
+		/// Initialize the Text to Speech Engine
+		/// </summary>
+		/// <param name="status">Status.</param>
+		public void OnInit(OperationResult status)
+		{
+			if (status == OperationResult.Success) {
+				var r = tts.SetLanguage(Java.Util.Locale.Us);
+				if (r == LanguageAvailableResult.MissingData || r == LanguageAvailableResult.NotSupported) {
+					Console.WriteLine("TTS Unsupported Language");
+					result = -1;
+				} else {
+					result = 0;
+				}
+			}
+			else {
+				Console.WriteLine("TTS Init Failed");
+				result = -1;
+			}
+		}
+
+		public async Task<int> GetPrice()
+		{
+			Model.BitCoinPrice bitcoin = await Model.BitCoinPrice.Load();
+
+			SayBTCPrice(bitcoin.last);
+				
+			return 0;
+		}
+
+		public void SayBTCPrice(string price) 
+		{
+			try 
+			{
+				this.RunOnUiThread(() => {
+					int spaceIndex = price.IndexOf(".");
+					if (spaceIndex != -1)
+						price = price.Substring(0, spaceIndex);
+
+					Card card = new Card(this.ApplicationContext);
+					card.SetText("1x Bitcoin = $" + price);
+					SetContentView(card.ToView());
+
+					string text = "The current bit coin price is " + price + " dollars";
+					if(result >= 0) {
+							tts.Speak(text, QueueMode.Flush, new Dictionary<string, string>());
+					}
+				});
+			}
+			catch (Exception e) {
+				Console.WriteLine("Error in SayBTCPrice:" + e.Message);
+			}
+		}
+    }
+}
+
---
+++ Model/BitCoinPrice.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using ServiceStack.Text;
+
+namespace GlassGetBitcoinPrice.Model
+{
+	public class BitCoinPrice
+	{
+		//	{"high": "989.60", "last": "962.00", "timestamp": "1386733158", "bid": "962.00", "volume": "19739.99168325", "low": "895.76", "ask": "962.49"}
+		public string high { get; set; }
+		public string last { get; set; }
+		public string timestamp { get; set; }
+		public string bid  { get; set; }
+		public string low  { get; set; }
+		public string ask  { get; set; }
+		public string volume { get; set; }
+
+		public BitCoinPrice()
+		{
+		}
+
+		public static async System.Threading.Tasks.Task<BitCoinPrice> Load()
+		{
+			Console.WriteLine("Requesting data from: https://coinbase.com/api/v1/currencies/exchange_rates");
+			HttpWebRequest request = new HttpWebRequest(new Uri("https://www.bitstamp.net/api/ticker/"));//"https://coinbase.com/api/v1/currencies/exchange_rates"));
+			request.Method 		= "GET";
+			request.ContentType = "application/json";
+
+			string res = null;
+
+			ServicePointManager.ServerCertificateValidationCallback = (p1, p2, p3, p4) => true;
+			try {
+				var response = await request.GetResponseAsync().ConfigureAwait(false);
+				using (var respStream = response.GetResponseStream()) {
+					res = readResponse(respStream);
+				}
+			} catch (WebException ex) {
+				if (ex.Response != null && ex.Status == WebExceptionStatus.ProtocolError)
+					using (var respStream = ex.Response.GetResponseStream()) {
+						res = readResponse(respStream);
+					}
+				else {
+					throw;
+				}
+			}
+			catch {
+				throw;
+			}
+
+			var ser = new JsonSerializer<Model.BitCoinPrice>();
+			var item = ser.DeserializeFromString(res);//data.ToJson());
+			return item;
+		}
+
+		private static string readResponse(Stream resultStream)
+		{
+			string result = null;
+			//pump the network stream into a local memory buffer.. 
+			using (MemoryStream localMem = new MemoryStream()) {
+				byte[] buffer = new byte[4096];
+				int read = resultStream.Read (buffer, 0, buffer.Length);
+				while (read > 0) {
+					localMem.Write (buffer, 0, read);
+					read = resultStream.Read(buffer, 0, buffer.Length);
+				}
+
+				//convert the full memory stream into UTF8 text
+				localMem.Seek (0, SeekOrigin.Begin);
+				using (var reader = new StreamReader(localMem, new System.Text.UTF8Encoding(false,false))) {
+					result = reader.ReadToEnd();
+				}
+			}
+			return result;
+		}
+
+	}
+}
+
